fdd6c294af80a4b7ebb8f6fdcdf2dc64be92c197
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-06-30 14:56:21 +0600

    todo creating

--- src/Components/Todo/TodoFrom.tsx
+++ src/Components/Todo/TodoFrom.tsx
@@ -1,19 +1,42 @@
-import React, { useContext } from 'react';
-import { TodoContext } from '../../Context/TodoProvider';
-import userTodoContext from './userTodoContext';
+import React, { useContext, useState } from "react";
+import { TodoContext } from "../../Context/TodoProvider";
+import userTodoContext from "./userTodoContext";
+import { TEvent } from "../../Interface/CommonTypes";
 
 const TodoFrom = () => {
-    
-    const {todoTitle} = userTodoContext()
+  const { state, dispatch } = userTodoContext();
+  const [task, setTask] = useState("");
 
-    console.log(todoTitle);
+  const handleSubmit = (e: TEvent) => {
+    e.preventDefault();
+    const todoData = {
+      id: Math.random().toString(32).substring(2, 7),
+      title: task,
+      isCompleted: false,
+    };
     
-    
-    return (
-        <div>
-            hello
-        </div>
-    );
+    dispatch({type:"addTodo", payload:todoData})
+  };
+
+  console.log(state);
+  
+  return (
+    <div>
+      <h1>Todo From </h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+        className="border border-red-400"
+          onBlur={(e) => setTask(e.target.value)}
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="todo"
+        />
+        <input className="border border-black m-3" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default TodoFrom;
--- src/Context/TodoProvider.tsx
+++ src/Context/TodoProvider.tsx
@@ -1,14 +1,41 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useReducer } from "react";
 
 type TTodoProps = {
   children: ReactNode;
 };
 
-export const TodoContext = createContext(undefined);
+interface ITodo {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
 
+interface IAction {
+  type: string;
+  payload: ITodo;
+}
+const initialState: ITodo[] = [];
+
+const reducer = (currentState: ITodo[], action: IAction) => {
+  switch (action.type) {
+    case "addTodo":
+      return [...currentState, action.payload];
+
+    default:
+      return currentState;
+  }
+};
+
+export const TodoContext = createContext<
+  { state: ITodo[]; dispatch: React.Dispatch<IAction> } | undefined
+>(undefined);
 const TodoProvider = ({ children }: TTodoProps) => {
+  // create useReducer
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   const info = {
-    todoTitle: "this is a todo title",
+    state,
+    dispatch,
   };
   return <TodoContext.Provider value={info}>{children}</TodoContext.Provider>;
 };
